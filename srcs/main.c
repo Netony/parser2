d44615171e670afb949f12c532b110f20992ffc5
Seunghyun Lee <seunghy2@c5r4s7.42seoul.kr>
2023-09-06 11:08:29 +0900

    seu.9.6.2

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: dajeon <dajeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 19:53:49 by dajeon            #+#    #+#             */
-/*   Updated: 2023/09/05 16:31:45 by seunghy2         ###   ########.fr       */
+/*   Updated: 2023/09/06 11:05:57 by seunghy2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,11 +102,3 @@ int	main(int argc, char **argv, char **envp)
 	ft_termexit(&info);
 	return (0);
 }
-/*
-1. main 문 인자 envp 를 envset() 함수에 넣어 환경변수 연결리스트 생성
-2. while 문 ( 파싱 -> piping() or exbuiltin() -> free(command는 free하지않는다))
-2-1. piping()이 아닌 exbuiltin()으로 들어가는 경우는
--> pipeline이 없으면서, builtin 함수일 때. (builtincheck()활용)
-3. 종료시그널 발생 시(exit, ctl + D), 1의 리스트 free()
-exit 일 때 , cmd_array 할당 해제 추가하기.
-*/
